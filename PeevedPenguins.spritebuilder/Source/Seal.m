58d30099059c07ba50304a5da6a77d43e8c5e838
Frank Navarro-Velasco <Frank@Franks-MacBook-Pro.local>
2014-06-24 15:21:30 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,23 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Frank Navarro-Velasco on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLOG(@"Penguin created");
+    }
+    
+    return self;
+}
+
+@end
