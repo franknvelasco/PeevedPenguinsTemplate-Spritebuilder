d8afbf654a43818e90cc26d5cab374a23b25f7be
Frank Navarro-Velasco <Frank@Franks-MacBook-Pro.local>
2014-06-25 11:56:17 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -138,14 +138,15 @@
 }
 
 - (void)ccPhysicsCollisionPostSolve:(CCPhysicsCollisionPair *)pair seal:(CCNode *)nodeA wildcard:(CCNode *)nodeB {
-    float energy = [pair totalKineticEnergy];
+    CCLOG(@"something collided with seal");
+    /*float energy = [pair totalKineticEnergy];
     
     // if energy is large enough, remove the seal
     if (energy > 5000.f) {
         [[_physicsNode space] addPostStepBlock:^{
             [self sealRemoved:nodeA];
         } key:nodeA];
-    }
+    }*/
 }
 
 - (void)sealRemoved:(CCNode *)seal {
