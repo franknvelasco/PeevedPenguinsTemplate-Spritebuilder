35258d547f5afb0d39cb820c3da87b775146c947
Frank Navarro-Velasco <Frank@Franks-MacBook-Pro.local>
2014-06-25 13:51:07 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/WaitingPenguin.m
@@ -0,0 +1,13 @@
+//
+//  WaitingPenguin.m
+//  PeevedPenguins
+//
+//  Created by Frank Navarro-Velasco on 6/25/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "WaitingPenguin.h"
+
+@implementation WaitingPenguin
+
+@end
